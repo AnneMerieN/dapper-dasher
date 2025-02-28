9024d6ac8947dd53d7aec0a42e843f522a51b187
Anne Merie Nguyen <annemerie001@gmail.com>
2025-02-28 09:48:09 -0800

    Animated scarfy

--- dasher.cpp
+++ dasher.cpp
@@ -2,6 +2,8 @@
     - velocity: speed and direction of movement
     - acceleration: change in velocity over time
     - gravity: constant acceleration pulling downward
+    - delta time: represents the time elapsed between two frames
+        - using delta time normalizes movement, so it remains the same regardless of frame rate
 */
 
 #include <raylib.h>
@@ -17,24 +19,34 @@ int main() {
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
     int velocity = 0; // Initial velocity (not moving)
-    const int jumpForce = -22; // Controls how high the jump is
-    const int gravity = 1; // Makes the character fall
+    const int jumpForce = -600; // Controls how high the jump is
+    const int gravity = 1000; // Makes the character fall
     bool isInAir{};
 
-    Texture2D scarfy = LoadTexture("textures/scarfy.png"); // compound data type
+    Texture2D scarfy = LoadTexture("textures/scarfy.png"); // load texture
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width / 6;
     scarfyRec.height = scarfy.height;
     scarfyRec.x = 0;
     scarfyRec.y = 0;
     Vector2 scarfyPos;
-    scarfyPos.x = windowWidth / 2 - scarfyRec.width/2;
+    scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
     scarfyPos.y = windowHeight - scarfyRec.height;
 
+    
 
-    SetTargetFPS(60);
+    // Animation frame
+    int frame = 0;
+    // Amount of time before we update the animation frame
+    const float updateTime(1.0/12.0);
+    float runningTime = 0;
+
+    SetTargetFPS(60); // sets target FPS, doesn't guarantee this value
 
     while(!WindowShouldClose()) {
+
+        // Calculate delta time
+        const float dT(GetFrameTime());
 
         // Start a new frame
         BeginDrawing();
@@ -47,7 +59,7 @@ int main() {
             isInAir = false;
         } else {
             // Only apply gravity when rectangle is in the air
-            velocity = velocity + gravity;
+            velocity = velocity + (gravity * dT);
             isInAir = true;
         }
 
@@ -57,7 +69,22 @@ int main() {
         }
 
 
-        scarfyPos.y = scarfyPos.y + velocity;
+        scarfyPos.y = scarfyPos.y + (velocity * dT);
+
+        // Update running time
+        runningTime = runningTime + dT;
+        if (runningTime >= updateTime) {
+            runningTime = 0;
+
+            // Update animation frame
+            scarfyRec.x = frame * scarfyRec.width;
+            frame++;
+
+            if (frame > 5) {
+                frame = 0;
+            }
+        }
+
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         // End the frame and present the new content
